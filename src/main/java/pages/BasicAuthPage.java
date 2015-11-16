1a3c1d8a453e95691faeea5a7cffe67599b77cbb
victortylikov <victortylikov@gmail.com>
2015-11-16 21:30:35 +0300

    Edited

--- src/main/java/pages/BasicAuthPage.java
+++ src/main/java/pages/BasicAuthPage.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 import static helpers.Helper.executeScriptCmd;
 
 public class BasicAuthPage {
-    private static final String CMD_PROGRAM = "./src/main/resources/ch_baw.exe";
-    private static File file = new File("./src/main/resources/data.txt");
+    private static final String CMD_PROGRAM = "./src/main/resources/ch_baw1.exe";
+    private static File file = new File("./src/main/resources/data1.txt");
     private static final String ACCOUNT_DATA = file.getAbsolutePath();
 
     public static void authorization() throws IOException {
